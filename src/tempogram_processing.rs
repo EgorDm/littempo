1b4c100c29c6dd2dd5bdadbb89ba70fdae2687ed
Egor Dmitriev <egordmitriev2@gmail.com>
2019-07-30 16:55:15 +0200

    Minor fixes for novelty curve. Made output match C++ version.

--- src/tempogram_processing.rs
+++ src/tempogram_processing.rs
@@ -21,11 +21,15 @@ pub fn calculate_novelty_curve<C, S, W, H, B>(s: &S, sr: f64, window_dim: W, hop
 	// TODO: use settings builder
 	// Create frequency spectrum. Normalize it. Cut off the noise
 	let w = window::hanning(window_dim);
+
 	let thresh = (10.).powf(-74. / 20.); // -74 db
 	let (stft, stft_sr) = calculate_stft(s, &w, hop_dim, true, sr);
 	let spe = stft.norm();
 	let spe_max = spe.maximum();
-	let spe = (spe / spe_max).clamp(thresh, 1.);
+	let mut spe = (spe / spe_max).clamp(thresh, 1.);
+	if let Some(compression_c) = log_compression {
+		spe = (spe * compression_c + 1.).log(1. + compression_c);
+	}
 
 	// Diff length
 	let diff_len = 0.3; // TODO move into settings
@@ -53,7 +57,7 @@ pub fn calculate_novelty_curve<C, S, W, H, B>(s: &S, sr: f64, window_dim: W, hop
 	let l_half_span = spe.col_count() - norm_len_half..spe.col_count();
 
 	let norm_filter_f_slice = norm_filter_sum.slice_cols(f_half_span.clone());
-	let norm_filter_f_slice_flipped = norm_filter_f_slice.flip_cols();
+	let norm_filter_f_slice_flipped = norm_filter_f_slice.flip_rows();
 
 	let mut bands_novelty_curve = ContainerRM::zeros(bands.row_dim(), spe.col_dim());
 
@@ -62,9 +66,7 @@ pub fn calculate_novelty_curve<C, S, W, H, B>(s: &S, sr: f64, window_dim: W, hop
 		let band_data = spe.slice_rows(bin[0] as usize..bin[1] as usize);
 
 		// Calculate band diff
-		let mut band_krn = ContainerRM::zeros(band_data.row_dim(), Dynamic::new(diff_len_half * 2 + band_data.col_count()));
-		band_krn.slice_cols_mut(diff_len_half..diff_len_half+band_data.col_count()).copy_from(&band_data);
-
+		let band_krn = pad_cols(&band_data, D!(diff_len_half), D!(diff_len_half), true);
 		let mut band_diff = conv2_same(&band_krn, &diff_filter).max(0.);
 		let mut band_diff = band_diff.slice_cols_mut(diff_len_half - 1..band_diff.col_count() - diff_len_half - 1);
 
@@ -74,8 +76,8 @@ pub fn calculate_novelty_curve<C, S, W, H, B>(s: &S, sr: f64, window_dim: W, hop
 		norm_curve.slice_cols_mut(f_half_span.clone()).div_assign(&norm_filter_f_slice_flipped);
 		norm_curve.slice_cols_mut(l_half_span.clone()).div_assign(&norm_filter_f_slice);
 
-		for mut row_band_diff in band_diff.as_row_slice_mut_iter() {
-			row_band_diff /= &norm_curve;
+		for mut band_diff_row in band_diff.as_row_slice_mut_iter() {
+			band_diff_row /= &norm_curve;
 		}
 
 		novelty_curve.copy_from(&sum_cols(&band_diff));
